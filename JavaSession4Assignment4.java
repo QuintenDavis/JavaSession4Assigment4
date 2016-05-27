b58b04438bd3666321220b3d3de5237fbe48529e
QuintenDavis <Quinten.R.Davis@gmail.com>
2016-05-27 09:19:00 -0400

    Add files via upload

---
+++ JavaSession4Assignment4.java
@@ -0,0 +1,16 @@
+
+public class JavaSession4Assignment4 {
+
+	public static void main(String[] args) {
+	
+		String string="abcdef";
+	      String reverse = new StringBuffer(string).
+	      reverse().toString();
+	      System.out.println("\nString before reverse:"
+	      +string);
+	      System.out.println("String after reverse:"
+	      +reverse);
+
+	}
+
+}
